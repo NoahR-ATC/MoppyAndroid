e41f6417c9fa5635554adcb77830a99354e8d70c
Noah R <reederm115@gmail.com>
2019-12-07 22:20:12 -0600

    Added SettingsDialogue class and layout
    Former-commit-id: 663a6b50a14a39da7bb804d3eba94e3a4ef72721

---
+++ app/src/main/java/com/moppyandroid/main/SettingsDialogue.java
@@ -0,0 +1,23 @@
+package com.moppyandroid.main;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+class SettingsDialogue extends Dialog implements DialogInterface.OnClickListener {
+    public SettingsDialogue(Activity activity) {
+        super(activity);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+
+    }
+}
